619e70d4a326abb1e312f32d13e1699a5ee5e2c9
ajmotylinski <drew.motylinski@gmail.com>
2022-01-29 08:38:10 -0600

    Queries and data output for retirement data

--- schema.sql
+++ schema.sql
@@ -48,13 +48,12 @@ CREATE TABLE titles (
 );
 
 CREATE TABLE dept_employees(
+	emp_no INT NOT NULL,	
 	dept_no varchar(4) NOT NULL,
-	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN Key (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	Primary Key (dept_no)
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
 );
 
