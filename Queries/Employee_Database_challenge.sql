10b3feb1dd519697279f83b4a02a9ae71d834310
ajmotylinski <drew.motylinski@gmail.com>
2022-01-29 19:12:13 -0600

    deliervable 2 complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+--Deliverable 1
+
 select e.emp_no, 
 e.first_name,
 e.last_name,
@@ -28,3 +30,21 @@ INTO retiring_titles
 from emp_list 
 group by  title 
 order by count(emp_no) DESC;
+
+--Deliverable 2
+SELECT distinct on (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ts.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ts
+ON (e.emp_no = ts.emp_no)
+where (de.to_date = '9999-01-01') and
+(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+order by e.emp_no, ts.to_date DESC;
