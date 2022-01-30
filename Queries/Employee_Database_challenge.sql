57130e0e18a3ef73ec69e04371b3012c69fdca96
ajmotylinski <drew.motylinski@gmail.com>
2022-01-29 18:40:05 -0600

    Deliverable 1 complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -4,7 +4,7 @@ e.last_name,
 ts.title,
 ts.from_date,
 ts.to_date
-INTO retire_emps
+INTO retirement_titles
 from employees as e
 LEFT JOIN titles as ts
 on (e.emp_no = ts.emp_no)
@@ -12,12 +12,19 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no
 ;
 
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
 
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
 INTO emp_list
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+FROM unique_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+select count(emp_no), 
+title 
+INTO retiring_titles
+from emp_list 
+group by  title 
+order by count(emp_no) DESC;
